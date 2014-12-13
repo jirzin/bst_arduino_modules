dd14a48a8b5595f0e7953a29e353e50a41b259a2
JR <ubu@ubu>
2014-12-13 16:30:30 +0100

    dimmer temperature testing code & hex file

--- bst_dimmer_000/bst_dimmer_000.ino
+++ bst_dimmer_000/bst_dimmer_000.ino
@@ -4,18 +4,21 @@
 // author: Bastlit
 
 // pin numbers
-int inPin = 2;
-int outPin = 13;
 
+int inPin = 2;
+int outPin = 9;
 
 // input signal smoothing
 int inX = 0;             // input signal
 int averageOf = 20;    // number of input values for average
 int xMax = 255;
 
+int fMax =255;
+//int state=0;
+int fSpeed = 40;
+int f = 0;
 
-int readingInterval = 200;
-
+//int readingInterval = 200;
 
 void setup(){
 
@@ -25,7 +28,17 @@ void setup(){
 }
 
 void loop(){
-
-
-
+  for(int n = 0; n < fMax; n++){
+    f = n;
+    analogWrite(outPin,f);
+    delay(fSpeed);
+  }
+  digitalWrite(outPin,f);
+  delay(10000);
+  for(int n = fMax; n > 0; n--){
+    f = n;
+    analogWrite(outPin,f);
+    delay(fSpeed);
+  }
+  delay(100);
 }
